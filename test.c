77df063e01125d43bfc864a258f9a88414d6f201
GitSign-VT <thombreviraj@gmail.com>
2024-12-03 10:31:44 +0000

    Printf example

--- test.c
+++ test.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("This is my first repository");
+    int i , count=0;
+    int sum=0;
+    for (i=0; i <=10;i++)
+    {
+	    count = count+ i ;
+	    sum = sum + count;
+	    printf("Sum %d, Counter %d, count %d\n", sum ,i, count);
+    }
+    printf("This is my first repository\n");
     return 0;
 }
